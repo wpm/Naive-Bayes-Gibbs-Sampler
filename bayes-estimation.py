50e9a1f2fdd5ce306f3ec0c0afe68f9f277231f8
W.P. McNeill <billmcn@gmail.com>
2011-10-01 10:57:57 -0700

    Use numpy.array Instead of Python list

--- bayes-estimation.py
+++ bayes-estimation.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
-from numpy.random import beta
+import numpy
+from numpy import array
+from numpy import empty
+from numpy import ones
+from numpy import zeros
 from numpy.random import dirichlet
 from numpy.random import sample
 
-def sample_from_distribution(distribution):
+
+def discrete_random_sample(distribution):
 	uniform = sample()
 	for i, p in enumerate(distribution):
 		if uniform < p:
@@ -16,25 +21,38 @@ def sample_from_distribution(distribution):
 def generate_corpus(c, v, r, n):
 	pi = dirichlet([1]*c, 1)[0]
 	thetas = dirichlet([1]*v, c)
-	corpus = []
-	labels = []
+	corpus = empty((n,v), numpy.int)
+	labels = empty(n, numpy.int)
 	for i in xrange(n):
-		c = sample_from_distribution(pi)
-		labels.append(c)
+		c = discrete_random_sample(pi)
+		labels[i] = c
 		theta = thetas[c]
-		w = [0]*v
+		w = zeros(v, numpy.int)
 		for j in xrange(r):
-			k = sample_from_distribution(theta)
+			k = discrete_random_sample(theta)
 			w[k] += 1
-		corpus.append(w)
+		corpus[i] = w
 	return pi, thetas, corpus, labels
 
 
 def initialize_estimation(hyp_pi, hyp_thetas, n):
 	pi = dirichlet(hyp_pi, 1)[0]
-	thetas = [dirichlet(hyp_theta, 1)[0] for hyp_theta in hyp_thetas]
-	labels = [sample_from_distribution(pi) for i in xrange(n)]
+	c, v = hyp_thetas.shape
+	thetas = empty(hyp_thetas.shape)	
+	for i in xrange(c):		
+		thetas[i] = dirichlet(hyp_thetas[i], 1)[0]	
+	labels = array([discrete_random_sample(pi) for i in xrange(n)])
 	return pi, thetas, labels
+	
+	
+def iterate_estimation(hyp_pi, hyp_thetas, pi, thetas, corpus, labels):
+	c = len(pi)		# number of categories
+	v = len(thetas)	# vocabulary size
+	# Get counts for the classes.
+	counts = [[0]*v]*c
+	for i, document in enumerate(corpus):
+		label = labels[i]
+		
 
 
 if __name__ == "__main__":
@@ -42,16 +60,16 @@ if __name__ == "__main__":
 	c = 2	# number of categories
 	v = 4	# vocabulary size
 	r = 100	# document length
-	n = 4	# dataset size
+	n = 5	# dataset size
 	
 	true_pi, true_theta, corpus, true_labels = generate_corpus(c, v, r, n)
 	print "true pi %s" % true_pi
-	print "true theta\n%s" % true_theta
+	print "true thetas\n%s" % true_theta
 	print "true labels %s" % true_labels
 	print "corpus\n%s" % corpus
 
 	print "\n\n"
-	pi, thetas, labels = initialize_estimation([1]*c, [[1]*v]*c, n)
+	pi, thetas, labels = initialize_estimation(ones(c, numpy.int), ones((c,v), numpy.int), n)
 	print "pi %s" % pi
 	print "thetas\n%s" % thetas
 	print "labels %s" % labels
