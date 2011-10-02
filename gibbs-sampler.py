3f5edbdf5b12c0bb94aabf8d29b57c0402b84c0c
W.P. McNeill <billmcn@gmail.com>
2011-10-02 16:01:40 -0700

    Rewrite Gibbs Sampler as an Object

--- gibbs-sampler.py
+++ gibbs-sampler.py
@@ -59,82 +59,93 @@ def generate_corpus(c, v, r, n, hyp_pi = None, hyp_thetas = None):
 	return pi, thetas, corpus, labels
 
 
-def initialize_gibbs_sampling(hyp_pi, hyp_thetas, corpus):
-	"""
-	Initialize a Gibbs sampling run
-	
-	@param hyp_pi: category hyperparameter
-	@type hyp_pi: array of float
-	@param hyp_thetas: word distribution in category hyperparameter
-	@type hyp_thetas: matrix of float
-	@param corpus: word frequencies per document
-	@type corpus: matrix of integers
-	@return: initial category distribution, word distributions, and labels
-	@rtype: tuple
-	"""
-	pi = dirichlet(hyp_pi, 1)[0]
-	c, v = hyp_thetas.shape
-	thetas = empty(hyp_thetas.shape)
-	for i in xrange(c):
-		thetas[i] = dirichlet(hyp_thetas[i], 1)[0]
-	labels = array([multinomial_sample(pi) for i in xrange(corpus.shape[0])])
-	return pi, thetas, labels
+class GibbsSampler(object):
+	def __init__(self, hyp_pi, hyp_thetas, corpus):
+		"""
+		Initialize the Gibbs sampler
 
+		@param hyp_pi: category hyperparameter
+		@type hyp_pi: array of float
+		@param hyp_thetas: word distribution in category hyperparameter
+		@type hyp_thetas: matrix of float
+		@param corpus: word frequencies per document
+		@type corpus: matrix of integers
+		"""
+		self.hyp_pi = hyp_pi
+		self.hyp_thetas = hyp_thetas
+		self.corpus = corpus		
 
-def iterate_gibbs_sampling(hyp_pi, hyp_thetas, thetas, labels, corpus):
-	"""
-	Perform a Gibbs sampling iteration
+	def __str__(self):
+		return "hyper pi\n%s\nhyper thetas\n%s\nthetas\n%slabels %s" % \
+			(self.hyp_pi, self.hyp_thetas, self.thetas, self.labels)
 	
-	@param hyp_pi: category hyperparameter
-	@type hyp_pi: array of float
-	@param hyp_thetas: word distribution in category hyperparameter
-	@type hyp_thetas: matrix of float
-	@param thetas: current word distribution in category parameter
-	@type thetas: matrix of float
-	@param labels: labels of the documents
-	@type labels: array of int
-	@param corpus: word frequencies per document
-	@type corpus: matrix of integers
-	@return: new category distribution, word distributions, and labels
-	@rtype: tuple
-	"""
-	documents = corpus.shape[0]		# corpus size
-	vocaublary = corpus.shape[1]	# vocabulary size
-	categories = hyp_pi.size		# number of categories
-	# Get class counts and word counts for the classes.
-	category_counts = empty(categories, int)
-	word_counts = empty((categories, vocaublary), int)
-	for category in xrange(categories):
-		category_counts[category] = count_nonzero(labels == category)
-		word_counts[category] = corpus[nonzero(labels == category)].transpose().sum(1)
+	def run(self, iterations = 10):
+		"""
+		Run the Gibbs sampler
+		
+		@param iterations: number of iterations to run
+		@type iterations: integer		
+		"""
+		self._initialize_gibbs_sampler()
+		for iteration in xrange(iterations):
+			self._iterate_gibbs_sampler()
+			yield iteration, self.thetas, self.labels
 	
-	# Estimate the new document labels.
-	for document in xrange(documents):
-		category = labels[document]
-		word_counts[category] -= corpus[document]
-		category_counts[category] -= 1
-		posterior_pi = empty(categories)
-		# Calculate label posterior for a single document.
+	def _initialize_gibbs_sampler(self):
+		"""
+		Initialize the Gibbs sampler
+		
+		This sets the initial values of the C{labels} and C{thetas} parameters.		
+		"""
+		pi = dirichlet(self.hyp_pi, 1)[0]
+		c, v = hyp_thetas.shape
+		self.thetas = empty(self.hyp_thetas.shape)
+		for i in xrange(c):
+			self.thetas[i] = dirichlet(self.hyp_thetas[i], 1)[0]
+		self.labels = array([multinomial_sample(pi) \
+			for i in xrange(self.corpus.shape[0])])
+	
+	def _iterate_gibbs_sampler(self):
+		"""
+		Perform a Gibbs sampling iteration.
+		
+		This updates the values of the C{labels} and C{thetas} parameters.		
+		"""
+		documents = self.corpus.shape[0]	# corpus size
+		vocaublary = self.corpus.shape[1]	# vocabulary size
+		categories = self.hyp_pi.size		# number of categories
+		# Get class counts and word counts for the classes.
+		category_counts = empty(categories, int)
+		word_counts = empty((categories, vocaublary), int)
 		for category in xrange(categories):
-			label_factor = \
-				(word_counts[category].sum() + hyp_pi[category] - 1.0)/ \
-				(word_counts.sum() + hyp_pi.sum() - 1.0)	# Writing 1.0 forces a cast
-															# to float.
-			word_factor = (thetas[category]**word_counts[category]).prod()
-			posterior_pi[category] = label_factor * word_factor
-		# Select a new label for the document.
-		posterior_pi /= posterior_pi.sum()
-		new_category = multinomial_sample(posterior_pi)
-		labels[document] = new_category
-		word_counts[new_category] += corpus[document]
-		category_counts[new_category] += 1
-	
-	# Estimate the new word count distributions.
-	t = word_counts + hyp_thetas
-	for theta_index in xrange(categories):
-		thetas[theta_index] = dirichlet(t[theta_index], 1)[0]
-	
-	return thetas, labels
+			category_counts[category] = count_nonzero(self.labels == category)
+			word_counts[category] = self.corpus[nonzero(self.labels == category)].transpose().sum(1)
+
+		# Estimate the new document labels.
+		for document in xrange(documents):
+			category = self.labels[document]
+			word_counts[category] -= corpus[document]
+			category_counts[category] -= 1
+			posterior_pi = empty(categories)
+			# Calculate label posterior for a single document.
+			for category in xrange(categories):
+				label_factor = \
+					(word_counts[category].sum() + self.hyp_pi[category] - 1.0)/ \
+					(word_counts.sum() + self.hyp_pi.sum() - 1.0)	# Writing 1.0 forces a cast
+																	# to float.
+				word_factor = (self.thetas[category]**word_counts[category]).prod()
+				posterior_pi[category] = label_factor * word_factor
+			# Select a new label for the document.
+			posterior_pi /= posterior_pi.sum()
+			new_category = multinomial_sample(posterior_pi)
+			self.labels[document] = new_category
+			word_counts[new_category] += self.corpus[document]
+			category_counts[new_category] += 1
+
+		# Estimate the new word count distributions.
+		t = word_counts + self.hyp_thetas
+		for theta_index in xrange(categories):
+			self.thetas[theta_index] = dirichlet(t[theta_index], 1)[0]
 
 
 if __name__ == "__main__":
@@ -150,19 +161,13 @@ if __name__ == "__main__":
 	print "true labels %s" % true_labels
 	print "corpus\n%s" % corpus
 	
+	# Create the Gibbs sampler.
 	hyp_pi = ones(c, int)			# uninformed label prior
 	hyp_thetas = ones((c,v), int)	# uninformed word prior
-	pi, thetas, labels = initialize_gibbs_sampling(hyp_pi, hyp_thetas, corpus)
-	print "\nInitialize"
-	print "pi %s" % pi
-	print "thetas\n%s" % thetas
-	print "labels %s" % labels
-
-	for i in xrange(3):
-		thetas, labels = iterate_gibbs_sampling(
-			hyp_pi, hyp_thetas,
-			thetas, labels,
-			corpus)
-		print "\nIteration %d" % (i+1)
+	sampler = GibbsSampler(hyp_pi, hyp_thetas, corpus)
+	
+	# Run the Gibbs sampler.
+	for iteration, thetas, labels in sampler.run(3):
+		print "\nIteration %d" % (iteration+1)
 		print "thetas\n%s" % thetas
 		print "labels %s" % labels
